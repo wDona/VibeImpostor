68ea2321d451dc5b2c941d493da6ae0307292bd9
wDona <donanferyt@gmail.com>
2026-05-29 17:42:14 +0200

    RELEASE

--- app/shared/src/jvmMain/kotlin/org/example/project/net/HttpClientFactory.jvm.kt
+++ app/shared/src/jvmMain/kotlin/org/example/project/net/HttpClientFactory.jvm.kt
@@ -4,13 +4,14 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.websocket.WebSockets
 import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.HttpTimeoutConfig
 
 actual fun createHttpClient(): HttpClient = HttpClient(CIO) {
     install(WebSockets)
     install(HttpTimeout) {
         requestTimeoutMillis = 10_000
         connectTimeoutMillis = 5_000
-        socketTimeoutMillis = 0  // No timeout para WebSocket
+        socketTimeoutMillis = HttpTimeoutConfig.INFINITE_TIMEOUT_MS // No timeout para WebSocket
     }
 }
 
